2adae40139a8c44a9ec3cf11f2d73b2e5c094ed8
ORIANEESTHER <orianenganga14@gmail.com>
2024-10-17 12:20:03 +0200

    Commit-initial

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { InMemoryStorage } from './Repositories/InMemoryStorage';
+import { CartController } from './Controllers/CartController';
+
+const app = express();
+app.use(express.json());
+
+const storage = new InMemoryStorage();
+const cartController = new CartController(storage);
+
+app.post('/cart/products', (req, res) => cartController.addProduct(req, res));
+app.get('/cart/total', (req, res) => cartController.getTotal(req, res));
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
